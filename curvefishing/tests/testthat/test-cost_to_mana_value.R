bcbe0912c433dbeaf6e89a59fe2e4f8572dc0ba1
Christopher Campbell <christopher.campbell@autotrader.co.uk>
2022-06-30 00:14:47 +0100

    tweak gitignore

--- curvefishing/tests/testthat/test-cost_to_mana_value.R
+++ curvefishing/tests/testthat/test-cost_to_mana_value.R
@@ -4,7 +4,7 @@ test_that("check cost_to_mana_value", {
         expected = c(2, 3, 3, 11))
 })
 
-test_that("check cost_to_mana_value", {
+test_that("check has_resource", {
     expect_equal(curvefishing:::has_resource(cost = c("RR", "2R", "RGU", "11"),
              resource = c(0, 0, 0, 1, 0)),
         expected = c(FALSE, TRUE, FALSE, TRUE))
